2b1a86c0200a1f43bcf111edf2083dd39878140a
Steve Tautonico <stautonico@gmail.com>
2024-07-05 04:38:51 -0400

    Init

---
+++ generate_prefered_images.py
@@ -0,0 +1,4 @@
+# Step 1: Ask for pokemon's id
+# Step 2: Ask for image 1 (repeat for 2,3, and 4)
+# Step 3: Download all the images into the pokemon's folder
+# Step 4: Convert them all to png
--- generate_sheet.py
+++ generate_sheet.py
@@ -10,6 +10,23 @@ center_text = workbook.add_format()
 center_text.set_align("center")
 center_text.set_align("vcenter")
 
+id_text = workbook.add_format({
+    # "border": 1,
+    "align": "center",
+    "valign": "vcenter",
+    # Make the text big
+    "font_size": 16
+})
+
+pokemon_text = workbook.add_format({
+    # "border": 1,
+    "align": "center",
+    "valign": "vcenter",
+    # Make the text big
+    "font_size": 16
+})
+
+
 livingdex.write(0, 0, "#", center_text)
 livingdex.write(0, 1, "Pokémon", center_text)
 livingdex.write(0, 2, "Top Pick", center_text)
@@ -22,8 +39,12 @@ for file in os.listdir("pokemon_data"):
         data = loads(f.read())
 
     row_index = int(file.split(".")[0])
-    livingdex.write(row_index, 0, data["id"])
-    livingdex.write(row_index, 1, data["name"].title())
-    livingdex(row_index, 2, )
+    livingdex.write(row_index, 0, data["id"], id_text)
+    livingdex.write(row_index, 1, data["name"].title(), pokemon_text)
+    livingdex.write(row_index, 2,
+                    f"=IMAGE(\"https://raw.githubusercontent.com/stautonico/tcg-livingdex/main/images/{data['id']}/1.jpg\", 2)")
+
+    livingdex.set_column_pixels(2, 2, 63*4)
+    livingdex.set_row_pixels(row_index, 88*4)
 
 workbook.close()
